d0871aab7134cde6fec0ee99969bed6c734689f1
Srujan Jujare <65067785+srujan375@users.noreply.github.com>
2025-07-23 16:22:19 +0530

    Minor change to set the model dynamically
    Removed thinking config set to 0, Now it should always take a thinking config parameter

--- aibridge/GoogleGenAIClient.py
+++ aibridge/GoogleGenAIClient.py
@@ -1,7 +1,7 @@
 import time
 from google import genai
 from google.genai import types
-from aibridge.llm import LLM  # Adjust import path as needed
+from aibridge.llm import LLM  # Relative import for same package
 from google.genai.types import ThinkingConfig
 from google.genai.types import (
     GenerateContentConfig,
@@ -77,10 +77,9 @@ class GoogleGenAIClient(LLM):
         })
         
         # Extract thinking_config or use default
-        default_thinking_config = ThinkingConfig(thinking_budget=0) # Default value
         thinking_config_params = self.vertexai_args.get("thinking_config", {})
         self.thinking_config = ThinkingConfig(
-            thinking_budget=thinking_config_params.get("thinking_budget", default_thinking_config.thinking_budget)
+            thinking_budget=thinking_config_params.get("thinking_budget")
         )
 
         self.safety_settings = [
@@ -133,7 +132,7 @@ class GoogleGenAIClient(LLM):
                 # else:
                     # In non-streaming mode, generate_content returns a single GenerationResponse object
                 responses = self.client.models.generate_content(
-                    model="gemini-2.5-flash-preview-04-17", # Hardcoded for now since no other model is supports thinking_config
+                    model=self.model_name, # Hardcoded for now since no other model is supports thinking_config
                     contents=[final_prompt],
                     config=GenerateContentConfig(
                         temperature=self.generation_config.get("temperature", 0.9),
@@ -141,7 +140,7 @@ class GoogleGenAIClient(LLM):
                         max_output_tokens=self.generation_config.get("max_output_tokens", 8000),
                         thinking_config=self.thinking_config
                     )
-                )
+                )   
                 total_text = responses.text if hasattr(responses, "text") else ""
 
                 # Update internal token counters (approximation)
